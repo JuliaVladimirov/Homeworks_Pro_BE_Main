3e55157298feaeb27a2fba11d943dcc5e3db9ce7
Yulia Vladimirov <julia.vladimirov@gmail.com>
2024-03-25 20:31:59 +0100

    HW_final

--- Homework_2024.03.21/src/main/java/org/example/Task1_Task2_IO.java
+++ Homework_2024.03.21/src/main/java/org/example/Task1_Task2_IO.java
@@ -3,7 +3,7 @@ package org.example;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
 
 public class Task1_Task2_IO {
     public static void main(String[] args) {
